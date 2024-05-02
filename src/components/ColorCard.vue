898dc872e771b6b0781400d33dd53208f0adceec
druchininss <druchininss@sovcombank.ru>
2024-05-02 13:23:26 +0700

    ADD dialog

---
+++ src/components/ColorCard.vue
@@ -0,0 +1,18 @@
+<script setup>
+const props = defineProps(["form", "color"])
+</script>
+
+<template>
+    <q-card
+        @click="form.color = color.id"
+        class="constructor__card"
+        :class="{ 'selected': form.color === color.id }"
+    >
+        <q-card-section>
+            <div class="column">
+                <q-avatar size="70px" :style="`background: ${color.color}`"></q-avatar>
+                <span class="text-center q-pt-md">{{  color.name  }}</span>
+            </div>
+        </q-card-section>
+    </q-card>
+</template>
---
+++ src/components/FlavourCard.vue
@@ -0,0 +1,18 @@
+<script setup>
+const props = defineProps(["form", "flavour"])
+</script>
+
+<template>
+    <q-card
+        @click="form.flavour = flavour.id"
+        class="constructor__card"
+        :class="{ 'selected': form.flavour === flavour.id }"
+    >
+        <q-card-section>
+            <div class="column">
+                <img class="constructor__card_img" :src="flavour.image" :alt="flavour.name" :key="flavour.id">
+                <span class="text-center">{{  flavour.name }}</span>
+            </div>
+        </q-card-section>
+    </q-card>
+</template>
